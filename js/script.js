13a6991f06166b7a4049a6ed62da4d5b1815d4eb
Innamorati <rikozhong04@gmail.com>
2024-05-22 21:07:01 +0700

    first update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-/*sdadad*/
+let areaProcess = "1/2 * "
+
+/* LUAS SEGITIGA */
+function calculate () {
+    let base = document.getElementById("inputBase").value;
+    let height = document.getElementById("inputHeight").value;
+
+    //calculate the area of the triangle
+    base = Number(base);
+    height = Number(height);
+    let areaofTriangle = 1/2 * base * height;
+    document.getElementById("process").textContent = areaProcess + base + ' * ' + height + ' = ';
+    document.getElementById("result").textContent = areaofTriangle;
+}
+
+function clearDisplay() {
+    let clear = ' ';
+    document.getElementById("myForm").reset();
+    document.getElementById("result").textContent = clear;
+    document.getElementById("process").textContent = clear;
+}
+
+
+/* KELILING SEGITIGA */
