257eeb361eb17e8ad9e1ea07e9a1767edb7020ee
Innamorati <rikozhong04@gmail.com>
2024-05-23 23:48:59 +0700

    ReVou Final

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
-let areaProcess = "1/2 * "
+let input = document.querySelector("input");
 
 /* LUAS SEGITIGA */
-function calculate () {
+function calculateArea() {
     let base = document.getElementById("inputBase").value;
     let height = document.getElementById("inputHeight").value;
-
-    //calculate the area of the triangle
+    let finalAnswer = document.getElementById("hasil");
+    let areaProcess = "1/2 * ";
+    
     base = Number(base);
     height = Number(height);
     let areaofTriangle = 1/2 * base * height;
-    document.getElementById("process").textContent = areaProcess + base + ' * ' + height + ' = ';
-    document.getElementById("result").textContent = areaofTriangle;
+    document.addEventListener("click", () => {
+        //check jika kosong
+        if (document.getElementById("inputBase").value.length == 0 || document.getElementById("inputHeight").value.length == 0 ) {
+            finalAnswer.textContent = "Mohon untuk memasukkan nilai tersebut!";
+        } else {
+            document.getElementById("hasil").textContent = areaProcess + base + ' * ' + height + ' = ' + areaofTriangle;
+        }
+    })
+    
+
 }
-
-function clearDisplay() {
-    let clear = ' ';
-    document.getElementById("myForm").reset();
-    document.getElementById("result").textContent = clear;
-    document.getElementById("process").textContent = clear;
-}
-
-
+ 
 /* KELILING SEGITIGA */
+function calculateKeliling () {
+    let a = document.getElementById("inputA").value;
+    let b = document.getElementById("inputB").value;
+    let c = document.getElementById("inputC").value;
+    let finalAnswer = document.getElementById("hasil2");
+
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+
+    let keliling = a + b + c;
+    document.addEventListener("click", () => {
+        //check jika kosong
+        if (document.getElementById("inputA").value.length == 0 || document.getElementById("inputB").value.length == 0 || document.getElementById("inputC").value.length == 0) {
+            finalAnswer.textContent = "Mohon untuk memasukkan nilai tersebut!";
+        } else {
+            document.getElementById("hasil2").textContent = a + ' + ' + b + ' + ' + c + ' = ' + keliling  ;
+        }
+    })
+}
+
+//membersihkan text perhitungan
+function clearDisplay() {
+    document.addEventListener("click", function() {
+        document.getElementById("hasil").textContent = ' ';
+        document.getElementById("hasil2").textContent = ' ';
+    })
+}
